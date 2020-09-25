b83bc66c49e3dd4b09d3a4ebd87c6cf3b1041ef6
chandler L <cbl0015@auburn.edu>
2020-09-25 17:37:22 -0400

    hotfix

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
+  var connection = mysql.createConnection({
     host: 'localhost',
     // port: 3306,
     user: 'root',
